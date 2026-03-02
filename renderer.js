c0178fe356ef0fbba6273ec28f365aa80aa92eeb
Ainul01-hub <ainulidris369@gmail.com>
2026-03-02 15:10:22 +0800

    renderer.js

--- renderer.js
+++ renderer.js
@@ -4,38 +4,59 @@ async function searchMeal() {
     const response = await fetch(
         `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`
     );
-
     const data = await response.json();
 
     if (!data.meals) {
         document.getElementById("mealResult").innerHTML = "Meal not found!";
         return;
     }
+    let output = "";
 
-    const meal = data.meals[0];
+    data.meals.forEach(meal => {
+    output += `
+        <div class="meal-card">
+            <h3>${meal.strMeal}</h3>
 
-    document.getElementById("mealResult").innerHTML = `
-        <h2>${meal.strMeal}</h2>
-        <img src="${meal.strMealThumb}" width="200">
-        <p><b>Category:</b> ${meal.strCategory}</p>
-        <p><b>Area:</b> ${meal.strArea}</p>
-        <p><b>Instructions:</b> ${meal.strInstructions}</p>
-        <button onclick="addToPlanner('${meal.strMeal}', '${meal.strCategory}')">
-            Add to Planner
-        </button>
+            <img src="${meal.strMealThumb}" 
+                 style="cursor:pointer;">
+
+            <p><b>Category:</b> ${meal.strCategory}</p>
+            <p><b>Area:</b> ${meal.strArea}</p>
+
+            <div class="instructions-box">
+                <p><b>Instructions:</b></p>
+                <p>${meal.strInstructions}</p>
+            </div>
+
+            <button class="btn-add"
+                onclick="addToPlanner('${meal.strMeal}','${meal.strCategory}')">
+                Add to Planner
+            </button>
+        </div>
     `;
-}
+});
 
+document.getElementById("mealResult").innerHTML = output;
+}
 function addToPlanner(name, category) {
+
     let meals = JSON.parse(localStorage.getItem("meals")) || [];
+
+    let calories = 400; 
+
+    if (category === "Chicken") calories = 450;
+    else if (category === "Beef") calories = 500;
+    else if (category === "Vegetarian") calories = 350;
+    else if (category === "Seafood") calories = 420;
 
     meals.push({
         name: name,
         category: category,
+        calories: calories,
         note: ""
     });
 
     localStorage.setItem("meals", JSON.stringify(meals));
 
-    alert("Meal Added Successfully!");
+    alert("Meal successfully added to planner!");
 }
